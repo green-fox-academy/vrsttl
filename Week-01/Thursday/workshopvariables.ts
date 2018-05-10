51e3345410f036573c9e6fbf3bd017fe1226b279
Attila.Veres <vrsttl@gmail.com>
2018-05-10 11:37:34 +0200

    a little practice with printing variables

---
+++ Week-01/Thursday/workshopvariables.ts
@@ -0,0 +1,9 @@
+let hello: string = "Hello world!";
+let int: number = 42;
+let float: number = 3.141592;
+let isittrue: boolean = true;
+
+console.log(hello);
+console.log(int);
+console.log(float);
+console.log(isittrue);
