48611288e6625b9a92427b4ae6b199b275b07dba
Attila.Veres <vrsttl@gmail.com>
2018-05-10 16:27:27 +0200

    thanks for this one.

---
+++ Week-01/Thursday/fizz-buzz.ts
@@ -0,0 +1,19 @@
+'use strict';
+export{};
+let j : string = 'Fizz';
+let k : string = 'Buzz';
+let l : string = 'FizzBuzz';
+for (let i : any = 1; i < 101; i++){
+  if (i % 3 === 0 && i % 5 === 0) {
+   console.log(l);
+  }
+   else if (i % 3 === 0) {
+     console.log(j);
+  } 
+   else if (i % 5 === 0) {
+     console.log(k);
+  }
+  else 
+
+  console.log(i);
+}
