fa53f2005778d459a40abc15369808ee9f432867
Attila.Veres <vrsttl@gmail.com>
2018-05-12 20:06:52 +0200

    chessboard solved

---
+++ Week-01/Thursday + Friday/chessboard.ts
@@ -0,0 +1,35 @@
+'use strict';
+export{};
+
+let lineCount : number = 8;
+let columnCount : number = 8;
+let spaceCount: number = 1;
+let percCount : number = 1;
+let charOutput : string = '';
+let i : number = 1;
+let evenLineIndicator : boolean = false;
+
+
+
+for (i; i <= lineCount; i++){
+  charOutput = '';
+  if (i % 2 === 1 || i === 1) {
+    evenLineIndicator = false;
+  } else {evenLineIndicator = true;}
+    if (evenLineIndicator === false){
+    charOutput += '% % % % ';
+  } 
+  else {charOutput += ' % % % %';}
+  console.log(charOutput);
+}
+// Create a program that draws a chess table like this
+//
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+// % % % %
+//  % % % %
+//
