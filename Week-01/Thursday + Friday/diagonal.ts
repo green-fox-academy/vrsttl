3b2cce5eab48a2b11965ad829a9a3c6c780a963c
Attila.Veres <vrsttl@gmail.com>
2018-05-12 19:45:59 +0200

    diagonal working

---
+++ Week-01/Thursday + Friday/diagonal.ts
@@ -0,0 +1,38 @@
+'use strict';
+export { };
+let lineCount: number = 17;
+let columnCount: number = lineCount - 1;
+let charOutput: string = '';
+let spaceCount: number = columnCount - 2;
+
+
+
+for (let i: number = 1; i <= lineCount; i++) {
+  charOutput = '';
+  if (i === 1 || i === lineCount) {
+    for (let k: number = 0; k <= columnCount; k++) {
+      charOutput += '%';
+    }
+  }
+  else if (i !== 1 || i !== lineCount) {
+    charOutput += '%';
+    for (let m: number = 0; m <= spaceCount; m++) {
+      if (m !== i - 2) { charOutput += ' '; }
+      else { charOutput += '%'; }
+    }
+    charOutput += '%';
+  }
+  console.log(charOutput);
+}
+
+// Write a program that draws a
+// square like this:
+//
+// %%%%%
+// %%  %
+// % % %
+// %  %%
+// %   %
+// %%%%%
+//
+// The square should have as many lines as lineCount is
