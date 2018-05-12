f5a1f4704863d9966363e70d5a99d8eabfb72997
Attila.Veres <vrsttl@gmail.com>
2018-05-12 20:08:08 +0200

    chessboard solved, unused variables discarded

--- Week-01/Thursday + Friday/chessboard.ts
+++ Week-01/Thursday + Friday/chessboard.ts
@@ -1,25 +1,20 @@
 'use strict';
-export{};
+export { };
 
-let lineCount : number = 8;
-let columnCount : number = 8;
-let spaceCount: number = 1;
-let percCount : number = 1;
-let charOutput : string = '';
-let i : number = 1;
-let evenLineIndicator : boolean = false;
+let lineCount: number = 8;
+let charOutput: string = '';
+let i: number = 1;
+let evenLineIndicator: boolean = false;
 
-
-
-for (i; i <= lineCount; i++){
+for (i; i <= lineCount; i++) {
   charOutput = '';
   if (i % 2 === 1 || i === 1) {
     evenLineIndicator = false;
-  } else {evenLineIndicator = true;}
-    if (evenLineIndicator === false){
+  } else { evenLineIndicator = true; }
+  if (evenLineIndicator === false) {
     charOutput += '% % % % ';
-  } 
-  else {charOutput += ' % % % %';}
+  }
+  else { charOutput += ' % % % %'; }
   console.log(charOutput);
 }
 // Create a program that draws a chess table like this
