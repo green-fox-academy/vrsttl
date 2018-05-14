48b4b9b12987b3cfe6303406ba2079738ff8345d
Attila.Veres <vrsttl@gmail.com>
2018-05-14 14:07:15 +0200

    change elements solved

---
+++ Week-02/Monday/change-element.ts
@@ -0,0 +1,9 @@
+'use strict';
+export { };
+let numList = [1, 2, 3, 8, 5, 6];
+const numlist2 = numList.map(func => numList[3] = 4);
+console.log(numlist2[3]);
+
+// -  Create an array named `numList` with the following content: `[1, 2, 3, 8, 5, 6]`
+// -  Change the 8 to 4 with the `.map` method 
+// -  Print the fourth element as a test
