ad675f54cf556e48f367a9f8da6e269266ede099
Attila.Veres <vrsttl@gmail.com>
2018-05-14 14:28:07 +0200

    appendind an 'a' to array solved

---
+++ Week-02/Monday/append-a-array.ts
@@ -0,0 +1,10 @@
+'use strict';
+export { };
+
+let animals = ['kuty', 'macsk', 'cic'];
+animals = animals.map(function (addA) {return addA + 'a'});
+console.log(animals);
+
+// -  Create an array variable named `animals`
+//    with the following content: `["kuty", "macsk", "cic"]`
+// -  Add all elements an `"a"` at the end
