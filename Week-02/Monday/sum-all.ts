b0fc96ec7f7c2deeade20a7a80ceb05f91546460
Attila.Veres <vrsttl@gmail.com>
2018-05-14 15:36:25 +0200

    sum of all elements added

---
+++ Week-02/Monday/sum-all.ts
@@ -0,0 +1,10 @@
+'use strict';
+export{ };
+
+let ai = [3, 4, 5, 6, 7];
+
+let sumOfElements = ai.reduce((a, b) => a + b, 0);
+console.log(sumOfElements);
+
+// -  Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+// -  Log the sum of the elements in `ai` to the console
