ec1455bd8b2366d04c1aaae8b74cf41a64560c0d
Attila.Veres <vrsttl@gmail.com>
2018-05-14 11:39:48 +0200

    doubling added

---
+++ Week-02/Monday/doubling.ts
@@ -0,0 +1,14 @@
+'use strict';
+export{};
+
+let baseNum : number = 123;
+
+function doubling() {
+  baseNum = baseNum * 2;
+  console.log(baseNum);
+}
+doubling();
+
+// -  Create a variable named `baseNum` and assign the value `123` to it
+// -  Create a function called `doubling` that doubles it's input parameter and returns with an integer
+// -  Log the result of `doubling(baseNum)` to the console
