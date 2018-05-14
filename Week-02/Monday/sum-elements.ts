985513e3e46feba7665bb7da8c18c300f42d3c8a
Attila.Veres <vrsttl@gmail.com>
2018-05-14 13:12:08 +0200

    sum elements upped, element will be array index+1 from now on

---
+++ Week-02/Monday/sum-elements.ts
@@ -0,0 +1,12 @@
+'use strict';
+export { };
+
+let r = [54, 23, 66, 12];
+
+let result : number = r[1]+r[2];
+
+console.log(result);
+
+
+// -  Create a variable named `r` with the following content: `[54, 23, 66, 12]`
+// -  Print the sum of the second and the third element
