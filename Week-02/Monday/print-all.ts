8efb71e7905f2c1aa0ad93c854297d045f60e99f
Attila.Veres <vrsttl@gmail.com>
2018-05-14 14:01:01 +0200

    prints all numbers in array w/ built-in foreach

---
+++ Week-02/Monday/print-all.ts
@@ -0,0 +1,13 @@
+'use strict';
+export { };
+
+let integers = [4, 5, 6, 7];
+let printThis = function(item){
+  console.log(item);
+};
+integers.forEach(printThis);
+
+// -  Create a variable named `integers` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `integers` to the console*
+// *hint: use a loop, console.log(integers) won't cut it
+// -  bonus for using the correct built in array method
