a14b0e569d87c82d6ccc29957c3e3f0278f3a40a
Attila.Veres <vrsttl@gmail.com>
2018-05-14 12:19:11 +0200

    third number printed

---
+++ Week-02/Monday/third.ts
@@ -0,0 +1,12 @@
+'use strict';
+export{};
+
+let magicNumbers = [1, 3, 5, 7];
+
+console.log(magicNumbers[3]);
+//this is the literal solution of the exercise. If the visual 3rd element
+//is needed, then the index in console.log has to be 2.
+
+// -  Create a variable named `magicNumbers`
+//    with the following content: `[1, 3, 5, 7]`
+// -  Print the third element of `magicNumbers`
