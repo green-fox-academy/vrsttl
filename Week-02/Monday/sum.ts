ae8569c9f34a6b032db96521add52f5128411f31
Attila.Veres <vrsttl@gmail.com>
2018-05-14 12:00:27 +0200

    sum works

---
+++ Week-02/Monday/sum.ts
@@ -0,0 +1,15 @@
+"use strict";
+export{};
+let param : number = 15;
+let result : number = 0;
+
+function sum(param){
+  for (let i : number = 0 ; i <= param; i++){
+    result = result + i;
+  }
+  console.log(result);
+}
+sum(param);
+
+// -  Write a function called `sum` that sum all the numbers until the given parameter
+// -  The function should return the result
