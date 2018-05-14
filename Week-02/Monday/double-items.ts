207aa1cbf08262d08e35010234a7bfa4f64c000f
Attila.Veres <vrsttl@gmail.com>
2018-05-14 14:51:02 +0200

    double items works

---
+++ Week-02/Monday/double-items.ts
@@ -0,0 +1,12 @@
+'use strict';
+export { };
+
+let drinks = ['Gin', 'Whiskey', 'Wine', 'Beer'];
+let drinks2 = drinks.map(element => {
+  element += element;
+  return element;
+});
+console.log(drinks2);
+// -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
