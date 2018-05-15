9edcdcf73d34e25665ae316a05df753bddee70e5
Attila.Veres <vrsttl@gmail.com>
2018-05-15 11:56:20 +0200

    reverse string uploaded

--- Week-02/Tuesday/reverse-string.ts
+++ Week-02/Tuesday/reverse-string.ts
@@ -2,11 +2,9 @@
 export { };
 let reversed: string = '.eslaf eb t\'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI';
 
-function reverse(){
-  reversed.split('').reverse().join();
-  return reversed;
+function reverse(reversed) {
+  return reversed.split('').reverse().join('');
 }
-
 console.log(reverse(reversed));
 
 export = reverse;
