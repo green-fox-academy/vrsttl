cba3c8d15e7e74d032d6b02b4fb5051012c82732
Attila.Veres <vrsttl@gmail.com>
2018-05-15 10:56:54 +0200

    output variable renamed to ex2, to be exported

---
+++ Week-02/Tuesday/simple-replace.ts
@@ -0,0 +1,12 @@
+'use strict';
+export {};
+let example: string = 'In a dishwasher far far away';
+let ex2 = example.replace('dishwasher', 'galaxy');
+console.log(ex2);
+
+export = ex2;
+
+
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
