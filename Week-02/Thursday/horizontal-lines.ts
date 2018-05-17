1560a28827b547dc65a35f56d4e20b2a70deb531
Attila.Veres <vrsttl@gmail.com>
2018-05-17 11:15:18 +0200

    horizontal lines in for loop function solved

---
+++ Week-02/Thursday/horizontal-lines.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+function lineDrawer(x: number, y: number) {  
+    ctx.strokeStyle = 'red';
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+    ctx.lineTo(x + 50, y);
+    ctx.stroke();
+    x += 50;
+    y += 50;
+  }
+
+  for (let i: number = 0; i < 3; i++) {
+    lineDrawer(i*20,i*20);    
+  }
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// create a line drawing function that takes 2 parameters:
+// the x and y coordinates of the line's starting point
+// and draws a 50 long horizontal line from that point.
+// draw 3 lines with that function. Use loop for that.
