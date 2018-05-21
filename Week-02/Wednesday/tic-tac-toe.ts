f8612872211b7b024d44b719a9d7d368a5d043a3
Attila.Veres <vrsttl@gmail.com>
2018-05-21 13:05:31 +0200

    tic-tac-toe incomplete, individualPlays array gets empty items from a .push() as a placeholder for linebreaks. Any useful input appreciated.

---
+++ Week-02/Wednesday/tic-tac-toe.ts
@@ -0,0 +1,63 @@
+'use strict';
+export { };
+
+declare function require(path: string): any;
+let fs = require('fs');
+let charEncoding = 'utf-8';
+let winnerStatus: string = '';
+
+function ticTacResult(filePath: string) {
+  try {
+    let matchContent: string = fs.readFileSync(filePath, charEncoding);
+    if (matchContent !== null) {
+      let matchContentArray: string[] = matchContent.split('');
+      matchContentArray.forEach(element => {
+        let bufferZone: any[] = element.split('');
+        let individualPlays: any[] = [];
+        bufferZone.forEach(element => {
+          if (element === 'X' || element === 'O') {
+            individualPlays.push(element);
+          }
+        });
+        if ((individualPlays[0] && individualPlays[1] && individualPlays[2]) ||
+          (individualPlays[3] && individualPlays[4] && individualPlays[5]) ||
+          (individualPlays[6] && individualPlays[7] && individualPlays[8]) ||
+          (individualPlays[0] && individualPlays[3] && individualPlays[6]) ||
+          (individualPlays[1] && individualPlays[4] && individualPlays[7]) ||
+          (individualPlays[2] && individualPlays[5] && individualPlays[8]) ||
+          (individualPlays[0] && individualPlays[4] && individualPlays[8]) ||
+          (individualPlays[2] && individualPlays[4] && individualPlays[6]) === 'X') {
+          winnerStatus = 'X wins'
+        }
+        else if ((individualPlays[0] && individualPlays[1] && individualPlays[2]) ||
+          (individualPlays[3] && individualPlays[4] && individualPlays[5]) ||
+          (individualPlays[6] && individualPlays[7] && individualPlays[8]) ||
+          (individualPlays[0] && individualPlays[3] && individualPlays[6]) ||
+          (individualPlays[1] && individualPlays[4] && individualPlays[7]) ||
+          (individualPlays[2] && individualPlays[5] && individualPlays[8]) ||
+          (individualPlays[0] && individualPlays[4] && individualPlays[8]) ||
+          (individualPlays[2] && individualPlays[4] && individualPlays[6]) === 'O') {
+          winnerStatus = 'O wins';
+        } else {
+          console.log(individualPlays);
+          winnerStatus = 'It\'s a draw...';
+        }
+      });
+    }
+  } catch (error) {
+    return 0;
+  }
+  console.log(winnerStatus);
+}
+ticTacResult('win-x.txt');
+
+// Write a function that takes a filename as string,
+// open and read it. The file data represents a tic-tac-toe
+// game result. Return 'X'/'O'/'draw' based on which player
+// has winning situation.
+//console.log(ticTacResult('win-o.txt'))
+// should print O
+//console.log(ticTacResult('win-x.txt'))
+// should print X
+//console.log(ticTacResult('draw.txt'))
+// should print draw
