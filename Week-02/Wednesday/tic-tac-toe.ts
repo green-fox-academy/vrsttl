a8a1ce9308afd22e99eba2bd84d48693f721a020
Attila.Veres <vrsttl@gmail.com>
2018-05-21 21:08:32 +0200

    empty array element no longer prints, however, false result is given. fuck this exercise already. I give up.

--- Week-02/Wednesday/tic-tac-toe.ts
+++ Week-02/Wednesday/tic-tac-toe.ts
@@ -10,7 +10,7 @@ function ticTacResult(filePath: string) {
   try {
     let matchContent: string = fs.readFileSync(filePath, charEncoding);
     if (matchContent !== null) {
-      let matchContentArray: string[] = matchContent.split('');
+      let matchContentArray: string[] = matchContent.split('\r\n');
       matchContentArray.forEach(element => {
         let bufferZone: any[] = element.split('');
         let individualPlays: any[] = [];
@@ -19,24 +19,26 @@ function ticTacResult(filePath: string) {
             individualPlays.push(element);
           }
         });
-        if ((individualPlays[0] && individualPlays[1] && individualPlays[2]) ||
-          (individualPlays[3] && individualPlays[4] && individualPlays[5]) ||
-          (individualPlays[6] && individualPlays[7] && individualPlays[8]) ||
-          (individualPlays[0] && individualPlays[3] && individualPlays[6]) ||
-          (individualPlays[1] && individualPlays[4] && individualPlays[7]) ||
-          (individualPlays[2] && individualPlays[5] && individualPlays[8]) ||
-          (individualPlays[0] && individualPlays[4] && individualPlays[8]) ||
+        if ((individualPlays[0] && individualPlays[1] && individualPlays[2]) === 'X' ||
+          (individualPlays[3] && individualPlays[4] && individualPlays[5]) === 'X' ||
+          (individualPlays[6] && individualPlays[7] && individualPlays[8]) === 'X' ||
+          (individualPlays[0] && individualPlays[3] && individualPlays[6]) === 'X' ||
+          (individualPlays[1] && individualPlays[4] && individualPlays[7]) === 'X' ||
+          (individualPlays[2] && individualPlays[5] && individualPlays[8]) === 'X' ||
+          (individualPlays[0] && individualPlays[4] && individualPlays[8]) === 'X' ||
           (individualPlays[2] && individualPlays[4] && individualPlays[6]) === 'X') {
+          console.log(individualPlays);
           winnerStatus = 'X wins'
         }
-        else if ((individualPlays[0] && individualPlays[1] && individualPlays[2]) ||
-          (individualPlays[3] && individualPlays[4] && individualPlays[5]) ||
-          (individualPlays[6] && individualPlays[7] && individualPlays[8]) ||
-          (individualPlays[0] && individualPlays[3] && individualPlays[6]) ||
-          (individualPlays[1] && individualPlays[4] && individualPlays[7]) ||
-          (individualPlays[2] && individualPlays[5] && individualPlays[8]) ||
-          (individualPlays[0] && individualPlays[4] && individualPlays[8]) ||
+        else if ((individualPlays[0] && individualPlays[1] && individualPlays[2]) === 'O' ||
+          (individualPlays[3] && individualPlays[4] && individualPlays[5]) === 'O' ||
+          (individualPlays[6] && individualPlays[7] && individualPlays[8]) === 'O' ||
+          (individualPlays[0] && individualPlays[3] && individualPlays[6]) === 'O' ||
+          (individualPlays[1] && individualPlays[4] && individualPlays[7]) === 'O' ||
+          (individualPlays[2] && individualPlays[5] && individualPlays[8]) === 'O' ||
+          (individualPlays[0] && individualPlays[4] && individualPlays[8]) === 'O' ||
           (individualPlays[2] && individualPlays[4] && individualPlays[6]) === 'O') {
+          console.log(individualPlays);
           winnerStatus = 'O wins';
         } else {
           console.log(individualPlays);
@@ -47,9 +49,10 @@ function ticTacResult(filePath: string) {
   } catch (error) {
     return 0;
   }
+
   console.log(winnerStatus);
 }
-ticTacResult('win-x.txt');
+ticTacResult('win-o.txt');
 
 // Write a function that takes a filename as string,
 // open and read it. The file data represents a tic-tac-toe
