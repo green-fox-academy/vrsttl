495cf3463fba69b8c64c63c0996cffc526f55d33
Attila.Veres <vrsttl@gmail.com>
2018-05-21 18:36:07 +0200

    reversed lines decrypted. can't add line breaks, sorry

---
+++ Week-02/Wednesday/reversed-lines.ts
@@ -0,0 +1,40 @@
+'use strict';
+export { };
+
+declare function require(path: string): any;
+let fs = require('fs');
+let charEncoding = 'utf-8';
+
+function readLog(filePath: string) {
+  try {
+    return fs.readFileSync('reversed-lines.txt', charEncoding);
+  } catch (error) {
+    console.log('Unable to read file: reversed-lines.txt');
+  }
+}
+
+function decrypt() {
+  try {
+    let rawText: any[] = readLog('reversed-lines.txt').split('\r\n');
+    let output: any[] = [];
+    let workingArray: any[] = [];
+    let letsPrintThis: string[] = [];
+    rawText.forEach(rawText => {
+      workingArray.push(rawText.split(''));
+    });
+
+    workingArray.forEach(workingArray => {
+      output.push(workingArray.reverse());
+    });
+
+    output.forEach(output => {
+      letsPrintThis += output.join('');
+    });
+    console.log(letsPrintThis);
+  }
+  catch (error) {
+    return 0;
+  }
+}
+decrypt();
+// Create a method that decrypts reversed-lines.txt
