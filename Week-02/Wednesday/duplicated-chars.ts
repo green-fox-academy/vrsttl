a1afad84a9eaaefd8e956414e8279ce8c91aa638
Attila.Veres <vrsttl@gmail.com>
2018-05-21 16:52:44 +0200

    duplicated chars solved

---
+++ Week-02/Wednesday/duplicated-chars.ts
@@ -0,0 +1,34 @@
+'use strict';
+export { };
+
+declare function require(path: string): any;
+let fs = require('fs');
+let charEncoding = 'utf-8';
+
+function readLog(filePath: string) {
+  try {
+    return fs.readFileSync('duplicated-chars.txt', charEncoding);
+  } catch (error) {
+    console.log('Unable to read file: log.txt');
+  }
+}
+
+function decrypt() {
+  try {
+    let rawText: string[] = readLog('duplicated-chars.txt').split('');
+    let output: any[] = [];
+    for (let i: number = 0; i <= rawText.length; i++) {
+      if (i % 2 !== 0) {
+        output.push(rawText[i]);
+      }
+    }
+    console.log(output.join(''));
+    return output.join();
+  }
+  catch (error) {
+    return 0;
+  }
+}
+decrypt();
+
+// Create a method that decrypts duplicated-chars.txt
