1902d02ce3f2d467185fac45a356ccf2c894a565
Attila.Veres <vrsttl@gmail.com>
2018-05-22 17:40:13 +0200

    dojo ascii template

---
+++ Week-02/Wednesday/ascii.js
@@ -0,0 +1,26 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+var L = parseInt(readline());
+var H = parseInt(readline());
+var T = readline();
+for (var i = 0; i < H; i++) {
+    var ROW = readline();
+}
+let letterRow = ' #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ### ';
+let letterRow2 ='# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   # ';
+let letterRow3 ='### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   ## ';
+let letterRow4 ='# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #       ';
+let letterRow5 ='# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  #  ';
+
+console.log(letterRow.substring(16,20));
+console.log(letterRow2.substring(16,20));
+console.log(letterRow3.substring(16,20));
+console.log(letterRow4.substring(16,20));
+console.log(letterRow5.substring(16,20));
+// Write an action using print()
+// To debug: printErr('Debug messages...');
+
+print();
