30d8eed7e5ca16523cf1b592dba36b2e9c42f17d
Attila.Veres <vrsttl@gmail.com>
2018-05-23 16:50:09 +0200

    sharpie solved

---
+++ Week-03/Wednesday/sharpie.ts
@@ -0,0 +1,27 @@
+'use strict';
+export { };
+
+class Sharpie {
+  color: string = '';
+  width: number = 0;
+  inkAmount: number = 0;
+
+  constructor(name: string) {
+    this.color = 'black';
+    this.inkAmount = 100;
+  }
+  use() {
+    this.inkAmount -= 10;
+  }
+}
+
+let newSharpie: Sharpie = new Sharpie('black');
+console.log(newSharpie);
+newSharpie.use();
+console.log(newSharpie);
+/*Create Sharpie class
+We should know about each sharpie their color (which should be a string), width (which will be a floating point number), inkAmount (another floating point number)
+When creating one, we need to specify the color and the width
+Every sharpie is created with a default 100 as inkAmount
+We can use() the sharpie objects
+which decreases inkAmount*/
