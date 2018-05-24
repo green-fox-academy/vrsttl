6cb5d726951e68abb549db399446dd50893813c7
Attila.Veres <vrsttl@gmail.com>
2018-05-24 10:28:58 +0200

    number adder solved

---
+++ Week-03/Thursday/number-adder.ts
@@ -0,0 +1,14 @@
+'use strict';
+export { };
+
+function adder(n: number): number {
+  if (n <= 0) {
+    return 0;
+  } else {
+    return (n + adder(n - 1))
+  };
+}
+
+console.log(adder(11));
+
+//Write a recursive function that takes one parameter: n and adds numbers from 1 to n.
