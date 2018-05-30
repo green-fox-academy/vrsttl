e813fc4950f6a27247cc5478c4ba8adc72bb8f6d
Attila.Veres <vrsttl@gmail.com>
2018-05-30 13:36:42 +0200

    Zoo appended

--- Week-04/Wednesday/zoo.ts
+++ Week-04/Wednesday/zoo.ts
@@ -4,7 +4,7 @@ import { Animal, Mammal, Reptile, Bird, Bear, Crocodile, Hummingbird } from './a
 let firstBear: Bear = new Bear('Koala');
 let firstCroc: Crocodile = new Crocodile('Crocodile');
 let firstHummingbird: Hummingbird = new Hummingbird('Parrot');
-
+console.log('How do you breed?');
 console.log(`A ${firstBear.getname()} breeds by ${firstBear.breed()}`);
 console.log(`A ${firstCroc.getname()} breeds by ${firstCroc.breed()}`);
 console.log(`A ${firstHummingbird.getname()} breeds by ${firstHummingbird.breed()}`);
