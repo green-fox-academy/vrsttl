baa9dbae27e8c631fab91bfb2af889b5764eed57
Attila.Veres <vrsttl@gmail.com>
2018-06-20 17:37:27 +0200

    rectangle done

---
+++ Week-06/Wednesday/svg-1/svg-1.js
@@ -0,0 +1,12 @@
+const rect = document.querySelector('#very_rectangle');
+rect.setAttribute('x', 50);
+rect.setAttribute('y', 50);
+rect.style.fill('tomato');
+
+/*<!-- You can work in the html or in a separate js file. Like:
+<script>
+    1) Select the rectangle by the ID: "very_rectangle"
+    2) Change the rectangle's position to x:50, y:50
+    3) Change its fill to tomato
+</script>
+OR*/
