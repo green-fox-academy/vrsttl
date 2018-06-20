460c7d4bef98c40ae894071c2c598bb8c342c6a7
Attila.Veres <vrsttl@gmail.com>
2018-06-20 17:15:48 +0200

    party solved

---
+++ Week-06/Wednesday/party/events-1.js
@@ -0,0 +1,21 @@
+document.querySelector('button').onclick = function () {
+  if (!(document.querySelector('body').classList.contains('party'))) {
+    document.querySelector('body').classList.add('party')
+  } else {
+    document.querySelector('body').classList.remove('party');
+  }
+};
+/*
+button.onclick = function () {
+  if (!(document.querySelector('button').classList.contains('party'))) {
+    document.querySelector('body').classList.add('party')
+  } else {
+    document.querySelector('body').classList.remove('party');
+  }
+};
+*/
+/*<!-- You can work in the html or in a separate js file. Like:
+<script>
+  Turn the party on and off by clicking the button. (the whole page)
+</script>
+OR*/
