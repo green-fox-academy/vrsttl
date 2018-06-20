37b946c43c9b7182665e92566fac2c98f09e92b0
Attila.Veres <vrsttl@gmail.com>
2018-06-20 14:02:43 +0200

    content 1 solved

---
+++ Week-06/Wednesday/content1/content-1.js
@@ -0,0 +1,15 @@
+body = document.querySelectorAll('p');
+/*for (i = 0; i < body.length; i++) {
+  body[i].textContent = body[body.length-1].textContent;
+}
+*/
+for (i = 0; i < body.length; i++) {
+  body[i].innerHTML = body[body.length-1].innerHTML;
+}
+
+/*<!-- You can work in the html or in a separate js file. Like:
+<script>
+  fill every paragraph with the last one's content.
+  fill every paragraph with the last one's content keeping the cat strong.
+</script>
+OR -->*/
