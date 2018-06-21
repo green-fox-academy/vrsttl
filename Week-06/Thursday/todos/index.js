97b6dad0635336a7822c390b3abbde10d3cdb72b
Attila Veres <vrsttl@gmail.com>
2018-06-21 14:15:54 +0200

    todos refactored

--- Week-06/Thursday/todos/index.js
+++ Week-06/Thursday/todos/index.js
@@ -16,9 +16,7 @@ app.get('/', (req, res) => {
   res.render('home', {
     title: 'Todo List!',    //passing content into home.ejs
     text: 'Things to do today:',
-    task1: todos[0],
-    task2: todos[1],
-    task3: todos[2],
+    task: todos,
   });
 });
 
