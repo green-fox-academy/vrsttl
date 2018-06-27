ca5e3e3fa3c0177167ce038b1f9121380bfc4778
Attila Veres <vrsttl@gmail.com>
2018-06-27 15:00:10 +0200

    books into table solved

--- Week-07/Wednesday/matrev/app.js
+++ Week-07/Wednesday/matrev/app.js
@@ -6,6 +6,12 @@ const mysql = require('mysql');
 const app = express();
 const PORT = 3000;
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(__dirname));
 
 const conn = mysql.createConnection({
@@ -17,7 +23,7 @@ const conn = mysql.createConnection({
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-})  
+})
 
 // app.get('/test', (req, res) => {
 //   let sql = 'SELECT * from author;';
@@ -56,23 +62,23 @@ app.get('/books', (req, res) => {
   });
 });
 
-// app.get('/allbooks', (req, res) => {
-//   let sql = 'SELECT book_name, cate_descrip, aut_name, pub_name, book_price from book_mast, category, author, publisher;';
+app.get('/allbooks', (req, res) => {
+  let sql = 'SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast LEFT JOIN author ON book_mast.aut_id = author.aut_id LEFT JOIN category ON book_mast.cate_id = category.cate_id LEFT JOIN publisher ON book_mast.pub_id = publisher.pub_id;';
 
-//   conn.query(sql, (err, rows) => {
-//     if (err) {
-//       console.log(err);
-//       res.status(500).send();
-//       //res.sendStatus(500);
-//       return;
-//     } else {
-//       console.log(sql);
-//       res.json({
-//         books: rows,
-//       });
-//     }
-//   });
-// });
+  conn.query(sql, (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+      //res.sendStatus(500);
+      return;
+    } else {
+      console.log(sql);
+      res.json({
+        books: rows,
+      });
+    }
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server up and running on port ${PORT}.`);
--- Week-07/Wednesday/matrev/index.js
+++ Week-07/Wednesday/matrev/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let http = new XMLHttpRequest();
-
+/*
 http.open('GET', 'http://localhost:3000/books', true);
 http.onload = () => {
   const response = JSON.parse(http.responseText);
@@ -15,9 +15,37 @@ http.onload = () => {
   }
 }
 http.send();
-/*
+*/
+
 http.open('GET', 'localhost:3000/allbooks', true);
 http.onload = () => {
   const response = JSON.parse(http.responseText);
 }
-http.send();*/
+
+http.open('GET', 'http://localhost:3000/allbooks', true);
+http.onload = () => {
+  const response = JSON.parse(http.responseText);
+  console.log(response);
+  let baseTable = document.querySelector('table');
+  for (let i = 0; i < response.books.length; i++) {
+    let oneRow = document.createElement('tr');
+    baseTable.appendChild(oneRow);
+    let block1 = document.createElement('td');
+    oneRow.appendChild(block1);
+    block1.innerText = response.books[i].book_name;
+    let block2 = document.createElement('td');
+    oneRow.appendChild(block2);
+    block2.innerText = response.books[i].cate_descrip;
+    let block3 = document.createElement('td');
+    oneRow.appendChild(block3);
+    block3.innerText = response.books[i].aut_name;
+    let block4 = document.createElement('td');
+    oneRow.appendChild(block4);
+    block4.innerText = response.books[i].pub_name;
+    let block5 = document.createElement('td');
+    oneRow.appendChild(block5);
+    block5.innerText = response.books[i].book_price;
+  }
+}
+
+http.send();
