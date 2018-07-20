c8f5ec8ce73572ca75a63770157cb71feb7a24b6
Attila Veres <vrsttl@gmail.com>
2018-07-20 18:04:27 +0200

    first robot tiles created

---
+++ summer-break-practice-01-react-app/robofriends/src/Robots.js
@@ -0,0 +1,74 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Alfa Bela',
+    username: 'Bela',
+    email: 'alfa@beta.com'
+  },
+  {
+    id: 2,
+    name: 'Gamma Denes',
+    username: 'Denes',
+    email: 'gamma@delta.com'
+  },
+  {
+    id: 3,
+    name: 'Epszilon Geza',
+    username: 'Geza',
+    email: 'epszilon@dzeta.com'
+  },
+  {
+    id: 4,
+    name: 'Eta Timea',
+    username: 'Timeja',
+    email: 'eta@theta.com'
+  },
+  {
+    id: 5,
+    name: 'Iota Kata',
+    username: 'Kata',
+    email: 'iota@kappa.com'
+  },
+  {
+    id: 6,
+    name: 'Lambda Mari',
+    username: 'Mari',
+    email: 'lambda@mu.com'
+  },
+  {
+    id: 7,
+    name: 'Nu Krisztina',
+    username: 'Kszti',
+    email: 'nu@kszi.com'
+  },
+  {
+    id: 8,
+    name: 'Omikron Peter',
+    username: 'Peti',
+    email: 'omikron@3.14.com'
+  },
+  {
+    id: 9,
+    name: 'Ro Szintia',
+    username: 'Szinti',
+    email: 'rho@sigma.com'
+  },
+  {
+    id: 10,
+    name: 'Tau Ubul',
+    username: 'Ubi',
+    email: 'tau@upszilon.com'
+  },
+  {
+    id: 11,
+    name: 'Fi Kalman',
+    username: 'Kalmi',
+    email: 'phi@khi.com'
+  },
+  {
+    id: 12,
+    name: 'Pszi Otto',
+    username: 'Otti',
+    email: 'pszi@omega.com'
+  }
+]
