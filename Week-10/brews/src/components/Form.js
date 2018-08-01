6a8f48c1c3b4edaf8fc8b51eafc2df4ec6f6594f
Attila Veres <vrsttl@gmail.com>
2018-08-01 11:33:51 +0200

    query acting weird through api

--- Week-10/brews/src/components/Form.js
+++ Week-10/brews/src/components/Form.js
@@ -3,11 +3,16 @@ import React from 'react';
 class Form extends React.Component {
   render() {
     return (
-      <form>
-      <input type='text' name='beername' placeholder='Type the name of the beer here...'/>
-      <input type='text' name='hops' placeholder='Type the name of the hops here...'/>
-      <input type='text' name='malt' placeholder='Type the name of the malt here...'/>
-      <input type='text' name='foodpair' placeholder='Type the food you want to pair it with here...'/>
+      <form onSubmit={this.props.getBeers}>
+        Name of the beer: <input type='text' name='beername' placeholder='Type the name of the beer here...' />
+        <br />
+        {/* Type of the hops: <input type='text' name='hops' placeholder='Type the name of the hops here...' />
+        <br />
+        Type of the malt: <input type='text' name='malt' placeholder='Type the name of the malt here...' />
+        <br />
+        Which food you want to pair it with: <input type='text' name='foodpair' placeholder='Type the food you want to pair it with here...' />
+        <br /> */}
+        <button>Show me all the beers I want</button>
       </form>
     );
   }
