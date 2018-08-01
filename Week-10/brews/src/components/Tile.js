e43a9321b29fdd449bbc7344c0a44d3171d998a2
Attila Veres <vrsttl@gmail.com>
2018-08-01 14:14:32 +0200

    api works

--- Week-10/brews/src/components/Tile.js
+++ Week-10/brews/src/components/Tile.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Tile = () => {
+const Tile = (props) => {
   return (
-    <div>
-      <img alt='beer' src='' />
+    <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='beer' src={props.imgurl} />
       <div>
-        <h2>Beer Name</h2>
-        <p>Defintion</p>
+        <h2>{props.name}</h2>
+        <p>{props.description}</p>
       </div>
     </div>
   );
--- Week-10/brews/src/components/Tilelist.js
+++ Week-10/brews/src/components/Tilelist.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Tile from './Tile';
 
-const TileList = () => {
-
+const TileList = (props) => {
+  return (
+    <div>
+      {
+        props.beers.map((element, i) => {
+          return (
+            <Tile
+              key={i}
+              name={element.name}
+              imgurl={element.image_url}
+              description={element.description}
+            />
+          );
+        })
+      }
+    </div>
+  );
 }
 
 export default TileList;
