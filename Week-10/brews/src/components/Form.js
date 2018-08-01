f7565519d364ddabaf1facba094786aa6cecbb5f
Attila Veres <vrsttl@gmail.com>
2018-08-01 13:27:32 +0200

    state set, ready to be passed down to tilelist

--- Week-10/brews/src/components/Form.js
+++ Week-10/brews/src/components/Form.js
@@ -4,14 +4,6 @@ class Form extends React.Component {
   render() {
     return (
       <form onSubmit={this.props.getBeers}>
-        Name of the beer: <input type='text' name='beername' placeholder='Type the name of the beer here...' />
-        <br />
-        {/* Type of the hops: <input type='text' name='hops' placeholder='Type the name of the hops here...' />
-        <br />
-        Type of the malt: <input type='text' name='malt' placeholder='Type the name of the malt here...' />
-        <br />
-        Which food you want to pair it with: <input type='text' name='foodpair' placeholder='Type the food you want to pair it with here...' />
-        <br /> */}
         <button>Show me all the beers I want</button>
       </form>
     );
